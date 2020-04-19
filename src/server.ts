19c982eeb2827b6fea5c642e63254381a06ddb12
dennisdt <dennistran714@gmail.com>
2020-04-18 18:19:19 -0700

    updated code for deploy

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
+import Jimp = require('jimp');
+
 (async () => {
 
   // Init the Express application
@@ -12,37 +14,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
   
+  // Image filtering Endpoint
+  // Returns a filtered image to the user
+  app.get( "/filteredimage", async ( req, res ) => {
+    let { image_url } = req.query;
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-  app.get("/filteredimage", async (req: Request, res: Response) => {
-    const { image_url }: { image_url: string } = req.query;
     if (!image_url) {
-      return res
-        .status(401)
-        .send("Please include an image_url in the query parameters");
+      return res.status(400).send("please input a valid image url");
     }
-    const filteredImage: string = await filterImageFromURL(image_url);
-    return res.sendFile(filteredImage, () => deleteLocalFiles([filteredImage]));
-  });
 
-  //! END @TODO1
-  
+    let filteredPath = await filterImageFromURL(image_url);
+    res.status(200).sendFile(filteredPath, () => { deleteLocalFiles([filteredPath]); });
+  } );
+
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
